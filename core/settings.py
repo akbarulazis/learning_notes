bdf66b272cd4c7bda608c78f5feed118066e9785
akbarulazis <73051141+akbarulazis@users.noreply.github.com>
2025-04-21 00:30:13 +0700

    change

--- core/settings.py
+++ core/settings.py
@@ -115,9 +115,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-STATIC_ROOT = [
-    BASE_DIR / "static"
-]
+STATIC_ROOT = BASE_DIR / "static"
+
 
 
 # Default primary key field type
